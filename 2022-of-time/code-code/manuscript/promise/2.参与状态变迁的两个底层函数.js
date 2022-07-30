b7ea5593b083d622b9463753fefe795ccda62293
aiyoudiao <newdiao@163.com>
2022-07-31 02:35:41 +0800

    feat: 完善手写promise的草稿

--- 2022-of-time/code-code/manuscript/promise/2.参与状态变迁的两个底层函数.js
+++ 2022-of-time/code-code/manuscript/promise/2.参与状态变迁的两个底层函数.js
@@ -11,5 +11,29 @@
  * ----------	---	---------------------------------------------------------  *
  */
 
+const PENDING = 0; // 挂起的状态
+const FULFILLED = 1; // 执行成功的状态
+const REJECTED = 2; // 执行失败的状态
 
+// 定义一个Promise函数，以他为构造函数，用来创建promise对象
+function Promise () {
+
+    let state = PENDING; // 存储当前promise的状态
+
+    let value = null; // 存储执行成功后的返回值 or 执行失败后的异常信息
+
+    const handlers = []; // 存储promise.then时传入的每一个回调函数(任务)
+
+    // 执行成功后，将状态变更为执行成功的状态，同时将执行结果返回值进行保存，存到value变量中
+    function fulfill (result) {
+        state = FULFILLED
+        value = result
+    }
+
+    // 执行失败后，将状态变更为执行失败的状态，同时将执行结果返回值进行保存，存到value变量中
+    function reject (error) {
+        state = REJECTED
+        value = error
+    }
+}
 
